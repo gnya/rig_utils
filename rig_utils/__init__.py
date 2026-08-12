a85f8f4cccdb0af7d20c9c24045b6a4b9ef3cf9e
gnya <gnya.motion@gmail.com>
2026-08-13 01:36:54 +0900

    add: init

---
+++ rig_utils/__init__.py
@@ -0,0 +1,16 @@
+bl_info = {
+    "name": "Rig Utils",
+    "author": "gnya",
+    "version": (0, 0, 1),
+    "blender": (3, 6, 0),
+    "description": "",
+    "category": "Utility",
+}
+
+
+def register():
+    pass
+
+
+def unregister():
+    pass
