90331ea741a2e76edbd1e25973f74a67ed1f1707
gnya <gnya.motion@gmail.com>
2026-08-16 01:07:43 +0900

    update: 複数の機能の実装
    * ペースト時にロックされたトランスフォームを維持するようになりました
    * ペースト時に自動キーイングが使えるようになりました

--- rig_utils/core/copy_paste.py
+++ rig_utils/core/copy_paste.py
@@ -2,7 +2,7 @@ import json
 from json.decoder import JSONDecodeError
 
 import bpy
-from bpy.types import Constraint, FCurve, Object
+from bpy.types import Constraint, FCurve, Object, PoseBone
 from mathutils import Matrix
 
 from .visibility import is_internal_bones
@@ -190,6 +190,67 @@ def _calc_depth_by_bone(obj: Object) -> dict[str, int]:
     return depth_by_bone
 
 
+# ロックされたトランスフォームの値を復元する
+def _restore_locked_transform(bone: PoseBone, matrix_basis: Matrix):
+    translation, quaternion, scale = matrix_basis.decompose()
+
+    for i, locked in enumerate(bone.lock_location):
+        if locked:
+            bone.location[i] = translation[i]
+
+    if bone.rotation_mode == "QUATERNION":
+        if not bone.lock_rotations_4d and bone.lock_rotation_w:
+            bone.rotation_quaternion[0] = quaternion[0]
+
+        for i, locked in enumerate(bone.lock_rotation):
+            if locked:
+                bone.rotation_quaternion[i + 1] = quaternion[i + 1]
+    elif bone.rotation_mode == "AXIS_ANGLE":
+        if not bone.lock_rotations_4d and bone.lock_rotation_w:
+            bone.rotation_axis_angle[0] = quaternion.angle
+
+        for i, locked in enumerate(bone.lock_rotation):
+            if locked:
+                bone.rotation_axis_angle[i + 1] = quaternion.axis[i]
+    else:
+        euler = quaternion.to_euler(bone.rotation_mode)
+
+        for i, locked in enumerate(bone.lock_rotation):
+            if locked:
+                bone.rotation_euler[i] = euler[i]
+
+    for i, locked in enumerate(bone.lock_scale):
+        if locked:
+            bone.scale[i] = scale[i]
+
+
+# 自動キーイングを利用している場合には自動でキーを打つ
+def _insert_auto_keyframes(obj: Object, bone_names: list[str]):
+    bones = obj.pose.bones
+
+    if bpy.context.tool_settings.use_keyframe_insert_auto:
+        match bpy.context.tool_settings.auto_keying_mode:
+            case "ADD_REPLACE_KEYS":
+                options = set()
+            case "REPLACE_KEYS":
+                options = {"INSERTKEY_REPLACE"}
+
+        for bone_name in bone_names:
+            if (bone := bones.get(bone_name)) is not None:
+                bone.keyframe_insert("location", group=bone.name, options=options)
+
+                if bone.rotation_mode == "QUATERNION":
+                    bone.keyframe_insert(
+                        "rotation_quaternion", group=bone.name, options=options
+                    )
+                else:
+                    bone.keyframe_insert(
+                        "rotation_euler", group=bone.name, options=options
+                    )
+
+                bone.keyframe_insert("scale", group=bone.name, options=options)
+
+
 # ボーンのトランスフォームをワールド座標系でペーストする
 def paste_bone_transform(obj: Object) -> bool:
     try:
@@ -213,6 +274,8 @@ def paste_bone_transform(obj: Object) -> bool:
     for depth in sorted(bones_by_depth):
         for bone_name in bones_by_depth[depth]:
             bone = bones[bone_name]
+            matrix_basis = bone.matrix_basis.copy()
+
             bone.matrix = obj.convert_space(
                 pose_bone=bone,
                 matrix=Matrix(matrices[bone_name]),
@@ -220,8 +283,10 @@ def paste_bone_transform(obj: Object) -> bool:
                 to_space="POSE",
             )
 
+            _restore_locked_transform(bone, matrix_basis)
+
         bpy.context.view_layer.update()
 
-    # TODO 自動キーイングを利用している場合には自動でキーを打つ
+    _insert_auto_keyframes(obj, matrices.keys())
 
     return True
